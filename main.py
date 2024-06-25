3754a6035a04ad6068674cc79057cb7724fa1f1d
navid <navid.yosufie4@gmail.com>
2024-06-26 02:26:50 +0330

    add functions

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from fastapi import FastAPI, Body, Depends
+from fastapi import FastAPI, Depends, Body
 from pydantic import BaseModel
 from typing import Annotated
 import requests
 
 app = FastAPI()
 
+
 class NobitexAPI:
     def __init__(self, base_url, token):
         self.base_url = base_url
         self.token = token
 
-    def get_wallet_balance(self, payload):
+    def get_wallet_balance(self, payload: dict):
         url = f'{self.base_url}users/wallets/balance'
         response = requests.post(url, headers=self.token, data=payload)
         if response.status_code == 200:
@@ -22,7 +23,8 @@ class NobitexAPI:
         response = requests.get(url, headers=self.token)
         return response.json()
 
-    def get_detail_order(self, status, type, srcCurrency, dstCurrency, details):
+    def get_detail_order(self, status: bool, type: str, srcCurrency: float | int, dstCurrency: float | int,
+                         details: int):
         url = f'{self.base_url}market/orders/list?'
         payload = {
             'status': status,
@@ -51,12 +53,13 @@ class NobitexAPI:
         response = requests.post(url, headers=self.token, data=payload)
         return response.json()
 
+
 class GetData(BaseModel):
     payload: dict
 
 
-
-nobitex_api = NobitexAPI(base_url="https://api.nobitex.ir/", token={'Authorization': 'Token *****'})
+nobitex_api = NobitexAPI(base_url="https://api.nobitex.ir/",
+                         token={'Authorization': f'Token fb4d99e9cd4650fdf36304b499e79b4cbca3cb28'})
 
 
 @app.post('/balance')
@@ -64,3 +67,16 @@ def get_wallet_balance(data: GetData = Body()):
     return nobitex_api.get_wallet_balance(data.payload)
 
 
+@app.post('/orderlist')
+def get_list_order(data: GetData = Body()):
+    return nobitex_api.get_list_order()
+
+
+@app.post('/order/detail')
+def get_detail_order(data: GetData = Body()):
+    return nobitex_api.get_detail_order(data.payload)
+
+
+@app.post('/setorder')
+def set_order(data: GetData = Body()):
+    return nobitex_api.set_order(data.payload)
