5239f6aaef008632d9daba70084bebb094808b33
navid <navid.yosufie4@gmail.com>
2024-06-25 20:11:35 +0330

    change name address function get_wallet_balance

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class GetData(BaseModel):
     toke: dict
     payload: dict
 
-@app.post('/home/')
+@app.post('/balance/')
 def get_wallet_balance(data: GetData = Body()):
     url = 'https://api.nobitex.ir/users/wallets/balance'
     response = requests.post(url=url, headers=data.toke, data=data.payload)
