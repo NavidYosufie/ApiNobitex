dcf2b1c3f93137ad6cf66ca2e41efef8c6abde98
navid <navid.yosufie4@gmail.com>
2024-06-25 20:05:49 +0330

    delete code

--- main.py
+++ main.py
@@ -1,26 +1,16 @@
 from fastapi import FastAPI, Body
 from typing import Union
 from pydantic import BaseModel
-import uvicorn
 import requests
 
 app = FastAPI()
-
-
-@app.get('/blog/{slug}')
-def blog(slug, name: str = 'f'):
-    return {'message': f'hi my name is {name}'}
-
-
-
-
 
 class Person(BaseModel):
     toke: dict
     payload: dict
 
 @app.post('/home/')
-def index(data: Person = Body()):
+def get_wallet_balance(data: Person = Body()):
     url = 'https://api.nobitex.ir/users/wallets/balance'
     response = requests.post(url=url, headers=data.toke, data=data.payload)
     return response.json()
