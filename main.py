d36553ccdefa6b370741560227dd5fb754f5071c
navid <navid.yosufie4@gmail.com>
2024-06-25 20:09:32 +0330

    change name class

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ import requests
 
 app = FastAPI()
 
-class Person(BaseModel):
+class GetData(BaseModel):
     toke: dict
     payload: dict
 
 @app.post('/home/')
-def get_wallet_balance(data: Person = Body()):
+def get_wallet_balance(data: GetData = Body()):
     url = 'https://api.nobitex.ir/users/wallets/balance'
     response = requests.post(url=url, headers=data.toke, data=data.payload)
     return response.json()
