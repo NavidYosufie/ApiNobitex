3175e80137f42333abc9f920df86fba6188caa23
navid <navid.yosufie4@gmail.com>
2024-06-30 05:06:48 +0330

    import file router in main

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from fastapi import FastAPI
+from router import NobitexApi_get
+from router import NobitexApi_post
 
-
-
+app = FastAPI()
+app.include_router(NobitexApi_post.router)
+app.include_router(NobitexApi_get.router)
 
 
 
--- router/NobitexApi_get.py
+++ router/NobitexApi_get.py
@@ -1,12 +1,12 @@
 from schema import InputSchema, OutputSchema
 from ActionNobitex import NobitexAPI
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, APIRouter
 
-app = FastAPI()
+router = APIRouter(prefix='/user')
 
-api = NobitexAPI('https://api.nobitex.ir/', {'Authorization': 'Token *****'})
+api = NobitexAPI('https://api.nobitex.ir/', {'Authorization': 'Token fb4d99e9cd4650fdf36304b499e79b4cbca3cb28'})
 
 
-@app.get('/profile', tags=['Profile user'])
+@router.get('/profile', tags=['Profile user'])
 def get_user_info():
     return api.get_user_info()
